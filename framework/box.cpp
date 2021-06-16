dcb163d136494788b3ecb8ad0aa92abac61fd9dd
Hannes <hannes.altmann22@gmail.com>
2021-06-16 18:27:40 +0200

    Task 5.2: Implemented Sphere and Box class and tested area and volume method each

--- framework/box.cpp
+++ framework/box.cpp
@@ -4,6 +4,18 @@ Box::Box(const glm::vec3 &min, const glm::vec3 &max) :
         min_{min},
         max_{max} {}
 
-float Box::area() const { return 0; }
+float Box::area() const {
+  float length = max_.x - min_.x;
+  float width = max_.y - min_.y;
+  float height = max_.z - min_.z;
+  float area = 2 * ( (length * width) + (length * height) + (width * height) );
+  return area;
+}
 
-float Box::volume() const {return 0;}
+float Box::volume() const {
+  float length = max_.x - min_.x;
+  float width = max_.y - min_.y;
+  float height = max_.z - min_.z;
+  float volume = length * width * height;
+  return volume;
+}
--- source/tests.cpp
+++ source/tests.cpp
@@ -6,10 +6,56 @@
 
 TEST_CASE("Testing area and volume methods on Sphere and Box objects", "[area&volume]")
 {
-  Sphere s{{1.f,2.f,34.f}};
-  float es = s.area();
+  SECTION("Sphere area")
+  {
+    Sphere s{{1.0f,1.0f,1.0f},4.0f};
+    REQUIRE(Approx(201.06f) == s.area());
 
-  REQUIRE(es > 1);
+    Sphere s1{{1.0f,1.0f,1.0f},0.0f};
+    REQUIRE(0 == s1.area());
+
+    Sphere s2{{1.0f,1.0f,1.0f},1.0f};
+    REQUIRE(Approx(12.57f).epsilon(0.01) == s2.area());
+  }
+
+  SECTION("Sphere volume")
+  {
+    Sphere s{{1.0f,1.0f,1.0f},4.0f};
+    REQUIRE(Approx(268.08f) == s.volume());
+
+    Sphere s1{{1.0f,1.0f,1.0f},0.0f};
+    REQUIRE(0 == s1.volume());
+
+    Sphere s2{{1.0f,1.0f,1.0f},1.0f};
+    REQUIRE(Approx(4.19f).epsilon(0.01) == s2.volume());
+  }
+
+  SECTION("Box area")
+  {
+    Box b{{0.0f,0.0f,0.0f},{0.0f,0.0f,0.0f}};
+    REQUIRE(0 == b.area());
+
+    Box b1{{1.0f,1.0f,1.0f},{3.0f,3.0f,3.0f}};
+    REQUIRE(24 == b1.area());
+
+    Box b3{{-2.0f,1.0f,2.0f},{3.0f,3.0f,6.0f}};
+    REQUIRE(76 == b3.area());
+  }
+
+  SECTION("Box volume")
+  {
+    Box b{{0.0f,0.0f,0.0f},{0.0f,0.0f,0.0f}};
+    REQUIRE(0 == b.volume());
+
+    Box b1{{1.0f,1.0f,1.0f},{3.0f,3.0f,3.0f}};
+    REQUIRE(8 == b1.volume());
+
+    Box b2{{-2.0f,1.0f,2.0f},{3.0f,3.0f,2.0f}};
+    REQUIRE(0 == b2.volume());
+
+    Box b4{{-2.0f,1.0f,2.0f},{3.0f,3.0f,6.0f}};
+    REQUIRE(40 == b4.volume());
+  }
 }
 
 int main(int argc, char *argv[])
