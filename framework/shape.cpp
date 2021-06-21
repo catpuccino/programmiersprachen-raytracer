a03ecc6781d9e86bbfa5d2104e27666ee62ce4b5
Hannes <hannes.altmann22@gmail.com>
2021-06-21 18:22:06 +0200

    Task 5.4: Implemented virtual print method for Shape objs and overloaded stream operator<< to print members of Shape objs

--- framework/shape.cpp
+++ framework/shape.cpp
@@ -1,5 +1,15 @@
 #include "shape.hpp"
 
-Shape::Shape(const std::string &str, const Color &clr) :
+Shape::Shape(std::string const& str, const Color &clr) :
         name_{str},
         color_{clr} {}
+
+std::ostream& Shape::print(std::ostream& os) const {
+  os << "Name: " << name_ << std::endl;
+  os << "Color: " << color_.r << ", " << color_.g << ", " << color_.b << std::endl;
+  return os;
+}
+
+std::ostream& operator<<(std::ostream& os, Shape const& s) {
+  return s.print(os);
+}
--- framework/shape.hpp
+++ framework/shape.hpp
@@ -4,6 +4,7 @@
 #include "color.hpp"
 #include <glm/vec3.hpp>
 #include <string>
+#include <iostream>
 
 class Shape {
   public:
@@ -11,9 +12,14 @@ class Shape {
 
     virtual float area() const = 0;
     virtual float volume() const = 0;
+
+    virtual std::ostream& print(std::ostream& os) const;
   private:
     std::string name_;
     Color color_;
 };
+
+std::ostream& operator<<(std::ostream& os, Shape const& s);
+
 
 #endif // #ifndef SHAPE_HPP
