e6abb81f92520d54c063b79d1275b5c4821948c3
buzario <tobi_friedel@gmx.de>
2021-08-16 15:03:34 +0200

    Add operator+ for struct Color; Add tone-mapping to renderer

--- source/raytracer.cpp
+++ source/raytracer.cpp
@@ -6,6 +6,7 @@
 #include <thread>
 #include <utility>
 #include <cmath>
+#include <string>
 
 //now single threaded again
 int main(int argc, char* argv[])
@@ -13,11 +14,11 @@ int main(int argc, char* argv[])
   unsigned const image_width = 800;
   unsigned const image_height = 600;
   std::string const filename = "./checkerboard.ppm";
-  std::string t_sdf_filepath{"D:/Ernst des Lebens/Uni/Informatik/Semester 2/Programmiersprachen/�bung/Raytracer/materials.sdf"};
+  std::string t_sdf_filepath{"D:/Ernst des Lebens/Uni/Informatik/Semester 2/Programmiersprachen/Uebung/Raytracer/materials.sdf"};
   std::string h_sdf_filepath{"D:/Nextcloud/Bauhaus Uni Weimar/SoSe_2021/Programmiersprachen/Belege/Beleg_6/materials.sdf"};
 
   Scene scene;
-  SDFParser::parse_scene(h_sdf_filepath,scene);
+  SDFParser::parse_scene(t_sdf_filepath,scene);
 
   Renderer renderer{image_width, image_height, filename, scene};
 
