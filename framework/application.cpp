d1a3406d20d0240567870be3e090a71172f6c1e1
Hannes <hannes.altmann22@gmail.com>
2021-09-01 22:48:02 +0200

    simpliefied Application::createAnim() and made it more efficient

--- framework/application.cpp
+++ framework/application.cpp
@@ -9,18 +9,24 @@ void Application::createCameraRotAnim(unsigned num_frames) {
 
   float degree_per_frame = 360.0f / num_frames;
 
+  // initialize scene requisites
+  Scene scene;
+  SDFToolbox::parse_scene("D:/Nextcloud/Bauhaus Uni Weimar/SoSe_2021/Programmiersprachen/Belege/Beleg_6/materials.sdf",
+                          scene);
+
   // create SDF file for each frame
   for (unsigned current_frame = 1; current_frame <= num_frames; ++current_frame) {
-    Scene new_scene;
-    if (current_frame > 1) {
-      //SDFToolbox::write_scene
-      SDFToolbox::write_scene(current_frame,"D:/Nextcloud/Bauhaus Uni Weimar/SoSe_2021/Programmiersprachen/Belege/Beleg_6/materials.sdf",
+
+    if (current_frame == 2) {
+      // write animation description in separate file
+      SDFToolbox::write_scene(current_frame,"D:/Nextcloud/Bauhaus Uni Weimar/SoSe_2021/Programmiersprachen/Belege/Beleg_6/animation_description.txt",
                              degree_per_frame);
     }
 
-    SDFToolbox::parse_scene("D:/Nextcloud/Bauhaus Uni Weimar/SoSe_2021/Programmiersprachen/Belege/Beleg_6/materials.sdf",
-                           new_scene);
-    renderer_.set_new_scene(new_scene);
+    // apply animation step to existing scene
+    SDFToolbox::parse_scene("D:/Nextcloud/Bauhaus Uni Weimar/SoSe_2021/Programmiersprachen/Belege/Beleg_6/animation_description.txt",
+                           scene);
+    renderer_.set_new_scene(scene);
     std::string filename = std::string("../../images/frame_") + std::to_string(current_frame) + ".ppm";
     renderer_.set_filename(filename);
     renderer_.render();
